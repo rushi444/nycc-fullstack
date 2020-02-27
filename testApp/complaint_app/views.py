00c92f9a025a299fec3cd1e170d5d1bbc5a61305
rushi444 <rushi44@gmail.com>
2020-02-27 15:33:47 -0800

    able to get all complaints in users district

--- testApp/complaint_app/views.py
+++ testApp/complaint_app/views.py
@@ -5,27 +5,47 @@ from rest_framework.response import Response
 from rest_framework import status
 # Create your views here.
 
+
 class ComplaintViewSet(viewsets.ModelViewSet):
-  http_method_names = ['get']
-  serializer_class = ComplaintSerializer
-  def list(self, request):
-    # Get all complaints from the user's district
-    return Response()
+    http_method_names = ['get']
+    serializer_class = ComplaintSerializer
+    queryset = Complaint.objects.all()
+
+    # def get_queryset(self):
+    #   user = self.request.user
+    #   user_profile = user.userprofile
+    #   user_district = user_profile.district
+    #   return Complaint.objects.filter(council_dist__endswith=user_district)
+    
+# Get all complaints from the user's district
+    def list(self, *args, **kwargs):
+        user = self.request.user
+        user_profile = user.userprofile
+        user_district = user_profile.district
+        complaints = self.queryset.filter(council_dist__endswith=user_district)
+        serializer_instance = self.serializer_class(complaints, many=True)
+        return Response(serializer_instance.data, status=status.HTTP_200_OK)
+
 
 class OpenCasesViewSet(viewsets.ModelViewSet):
-  http_method_names = ['get']
-  def list(self, request):
-    # Get only the open complaints from the user's district
-    return Response()
+    http_method_names = ['get']
+
+    def list(self, request):
+        # Get only the open complaints from the user's district
+        return Response()
+
 
 class ClosedCasesViewSet(viewsets.ModelViewSet):
-  http_method_names = ['get'] 
-  def list(self, request):
-    # Get only complaints that are close from the user's district
-    return Response()
-    
+    http_method_names = ['get']
+
+    def list(self, request):
+        # Get only complaints that are close from the user's district
+        return Response()
+
+
 class TopComplaintTypeViewSet(viewsets.ModelViewSet):
-  http_method_names = ['get']
-  def list(self, request):
-    # Get the top 3 complaint types from the user's district
-    return Response()
+    http_method_names = ['get']
+
+    def list(self, request):
+        # Get the top 3 complaint types from the user's district
+        return Response()
