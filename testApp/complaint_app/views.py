79b4f95e47cc74bbc5f609b09b176bf1613dedfb
rushi444 <rushi44@gmail.com>
2020-02-28 15:15:42 -0800

    backend mvp features now working

--- testApp/complaint_app/views.py
+++ testApp/complaint_app/views.py
@@ -4,6 +4,7 @@ from .serializers import UserSerializer, UserProfileSerializer, ComplaintSeriali
 from rest_framework.response import Response
 from rest_framework import status
 from django.db.models import Count
+import operator
 # Create your views here.
 
 
@@ -25,6 +26,7 @@ class ComplaintViewSet(viewsets.ModelViewSet):
         user_district = user_profile.district
         complaints = self.queryset.filter(account__endswith=user_district)
         serializer_instance = self.serializer_class(complaints, many=True)
+        
         return Response(serializer_instance.data, status=status.HTTP_200_OK)
 
 
@@ -38,7 +40,8 @@ class OpenCasesViewSet(viewsets.ModelViewSet):
         user = self.request.user
         user_profile = user.userprofile
         user_district = user_profile.district
-        openComplaints = self.queryset.filter(account__endswith=user_district).filter(closedate__isnull=True)
+        openComplaints = self.queryset.filter(
+            account__endswith=user_district).filter(closedate__isnull=True)
         serializer_instance = self.serializer_class(openComplaints, many=True)
         return Response(serializer_instance.data, status=status.HTTP_200_OK)
 
@@ -62,14 +65,33 @@ class ClosedCasesViewSet(viewsets.ModelViewSet):
 
 class TopComplaintTypeViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
-    serializer_class = ComplaintSerializer
+    # serializer_class = ComplaintSerializer
     queryset = Complaint.objects.all()
-
     def list(self, request):
-        # Get the top 3 complaint types from the user's district
         user = self.request.user
         user_profile = user.userprofile
         user_district = user_profile.district
-        topComplaints = self.queryset.filter(account__endswith=user_district).annotate(
-            tc=Count('complaint-type')).order_by('-tc')
-        return Response()
+        allDistrictComplaints = Complaint.objects.filter(
+            account__endswith=user_district)
+        topComplaints = [*allDistrictComplaints.values('complaint_type').order_by('complaint_type').annotate(cnt=Count('complaint_type'))
+        .values('complaint_type', 'cnt').order_by('-cnt')[:3]] 
+        # topComplaints = {}
+        # for complaint in allDistrictComplaints:
+        #     if complaint.complaint_type in topComplaints:
+        #         topComplaints[complaint.complaint_type] += 1
+        #     else:
+        #         topComplaints[complaint.complaint_type] = 1
+        # sortedTopComplaints = sorted(
+        #     topComplaints.items(), key=operator.itemgetter(1), reverse=True)
+        # print(sortedTopComplaints[slice(0, 3)])
+        return Response(topComplaints)
+
+    # def list(self, request):
+    #     # Get the top 3 complaint types from the user's district
+    #     user = self.request.user
+    #     user_profile = user.userprofile
+    #     user_district = user_profile.district
+    #     topComplaints = self.queryset.filter(account__endswith=user_district)
+    #     finalTopComplaints = topComplaints.values.annotate(
+    #         tc=Count('complaint_type')).order_by('-tc')
+    #     return Response(finalTopComplaints, status=status.HTTP_200_OK)
