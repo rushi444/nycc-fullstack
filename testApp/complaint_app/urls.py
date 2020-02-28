007862159844faa2b5cf96a823bf26aafebe6586
rushi444 <rushi44@gmail.com>
2020-02-28 11:57:46 -0800

    help

--- testApp/complaint_app/urls.py
+++ testApp/complaint_app/urls.py
@@ -3,10 +3,9 @@ from rest_framework import routers
 from .views import ComplaintViewSet, OpenCasesViewSet, ClosedCasesViewSet, TopComplaintTypeViewSet
 
 router = routers.SimpleRouter()
-router.register(r'', ComplaintViewSet, base_name='complaint')
-router.register(r'openCases', OpenCasesViewSet, base_name='openCases')
-router.register(r'closedCases', ClosedCasesViewSet, base_name='closedCases')
-router.register(r'topComplaints', TopComplaintTypeViewSet, base_name='topComplaints')
-urlpatterns = [
-]
-urlpatterns += router.urls
+router.register(r'', ComplaintViewSet, 'complaint')
+router.register(r'opencases', OpenCasesViewSet, 'openCases')
+# router.register(r'closedCases', ClosedCasesViewSet, base_name='closedCases')
+# router.register(r'topComplaints', TopComplaintTypeViewSet, base_name='topComplaints')
+#urlpatterns = []
+urlpatterns = router.urls
--- testApp/complaint_app/views.py
+++ testApp/complaint_app/views.py
@@ -3,6 +3,7 @@ from .models import UserProfile, Complaint
 from .serializers import UserSerializer, UserProfileSerializer, ComplaintSerializer
 from rest_framework.response import Response
 from rest_framework import status
+from django.db.models import Count
 # Create your views here.
 
 
@@ -16,36 +17,60 @@ class ComplaintViewSet(viewsets.ModelViewSet):
     #   user_profile = user.userprofile
     #   user_district = user_profile.district
     #   return Complaint.objects.filter(council_dist__endswith=user_district)
-    
-# Get all complaints from the user's district
-    def list(self, *args, **kwargs):
+
+    def list(self, request):
+        # Get all complaints from the user's district
         user = self.request.user
         user_profile = user.userprofile
         user_district = user_profile.district
-        complaints = self.queryset.filter(council_dist__endswith=user_district)
+        complaints = self.queryset.filter(account__endswith=user_district)
         serializer_instance = self.serializer_class(complaints, many=True)
         return Response(serializer_instance.data, status=status.HTTP_200_OK)
 
 
 class OpenCasesViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
+    serializer_class = ComplaintSerializer
+    queryset = Complaint.objects.all()
+    print('hi')
 
     def list(self, request):
         # Get only the open complaints from the user's district
-        return Response()
+        user = self.request.user
+        user_profile = user.userprofile
+        user_district = user_profile.district
+        openComplaints = self.queryset.filter(account__endswith=user_district).filter(closedate__isnull=True)
+        serializer_instance = self.serializer_class(openComplaints, many=True)
+        return Response('hi')
 
 
 class ClosedCasesViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
+    serializer_class = ComplaintSerializer
+    queryset = Complaint.objects.all()
 
     def list(self, request):
         # Get only complaints that are close from the user's district
-        return Response()
+        user = self.request.user
+        user_profile = user.userprofile
+        user_district = user_profile.district
+        closedComplaints = self.queryset.filter(
+            account__endswith=user_district).filter(closedate__isnull=False)
+        serializer_instance = self.serializer_class(
+            closedComplaints, many=True)
+        return Response(serializer_instance, status=status.HTTP_200_OK)
 
 
 class TopComplaintTypeViewSet(viewsets.ModelViewSet):
     http_method_names = ['get']
+    serializer_class = ComplaintSerializer
+    queryset = Complaint.objects.all()
 
     def list(self, request):
         # Get the top 3 complaint types from the user's district
+        user = self.request.user
+        user_profile = user.userprofile
+        user_district = user_profile.district
+        topComplaints = self.queryset.filter(account__endswith=user_district).annotate(
+            tc=Count('complaint-type')).order_by('-tc')
         return Response()
